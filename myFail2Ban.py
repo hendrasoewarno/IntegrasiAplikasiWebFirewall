de0fa44c113836b7a43519232330adfc6e8fa421
hendrasoewarno <hendrasoewarno@gmail.com>
2021-10-09 21:25:11 +0700

    Update myFail2Ban.py

--- myFail2Ban.py
+++ myFail2Ban.py
@@ -27,31 +27,39 @@ class Detector:
 		self.suspects = {}
 	
 	#menambah pola dan bobot
-	def addPattern(self, pattern, weight):
-		self.patterns[pattern]=weight
+	def addPattern(self, pattern, data):
+		self.patterns[pattern]=data
 
 	#line = baris pada log untuk dideteksi keberadaan pattern
 	def detect(self, line):
 		retVal=0
 		ipaddr=None
 		score=0
+		desc=""
 		#jika ada pattern
 		for pattern in self.patterns:
 			found = re.search(pattern, line)
 			if found:
 				ipaddr = found.group(1)
-				score += self.patterns[pattern]
+				score += self.patterns[pattern]["score"]
+				desc += self.patterns[pattern]["name"] + "(" + str(self.patterns[pattern]["score"]) + ")\n"
 			
 		if ipaddr and score>0:
 			#jika IP Address pernah suspect
 			if ipaddr in self.suspects:
-				self.suspects[ipaddr]+=score
+				state = self.suspects[ipaddr]
+				state["score"]+= score
+				state["desc"]+= desc
+				self.suspects[ipaddr]=state
 			else:
-				self.suspects[ipaddr]=score
-				
-			#jika IP Address tercatat melampaui limit					
-			if self.suspects[ipaddr] >= self.limit:
-				blacklist(ipaddr, self.chain)
+				state = {"score":score, "desc":desc}
+				self.suspects[ipaddr]=state
+
+			print(self.suspects)	
+			#jika State dari IP Address tercatat melampaui limit					
+			if state["score"] >= self.limit:
+				blacklist(ipaddr, self.chain, state["desc"])
+				del self.suspects[ipaddr]
 				retVal=1
 
 		return retVal
@@ -64,7 +72,7 @@ class LogFilePoll:
 			self.detector = detector
 
 		def addFile(self, file):
-            #Tail -F /var/log/apache2/ssl_access.log
+			#Tail -F /var/log/apache2/ssl_access.log
 			f = subprocess.Popen(['tail','-F', file], \
 					stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 			p = select.poll()
@@ -83,28 +91,64 @@ class LogFilePoll:
 jailed = []
 ignoreIP = ["127.0.0.1","::1"]
 
+def ipToBinary(ip):
+        octet_list_int = ip.split(".")
+        octet_list_bin = [format(int(i), '08b') for i in octet_list_int]
+        binary = ("").join(octet_list_bin)
+        return binary
+
+def getAddrNetwork(address, net_size):
+        #Convert ip address to 32 bit binary
+        ip_bin = ipToBinary(address)
+        #Extract Network ID from 32 binary
+        network = ip_bin[0:32-(32-net_size)]
+        return network
+
+def ipInPrefix(ip_address, prefix):
+        #CIDR based separation of address and network size
+        [prefix_address, net_size] = prefix.split("/")
+        #Convert string to int
+        net_size = int(net_size)
+        #Get the network ID of both prefix and ip based net size
+        prefix_network = getAddrNetwork(prefix_address, net_size)
+        ip_network = getAddrNetwork(ip_address, net_size)
+        return ip_network == prefix_network
+
+def isIgnoreIP(ipaddr):
+        for ip in ignoreIP:
+                if "/" in ip:
+                        if ipInPrefix(ipaddr, ip):
+                                return True
+                else:
+                        if ipaddr==ip:
+                                return True
+        return False
+
 #ipaddr = IP Address yang akan diblacklist
 #chain = Chain iptables
-def blacklist(ipaddr, chain):
+def blacklist(ipaddr, chain, description):
 	now = datetime.now()
-	if ipaddr in ignoreIP:
+	if isIgnoreIP(ipaddr):
 		print("ignoreIP " + ipaddr)
 	elif ipaddr in jailed:
 		print("jailed " + ipaddr)
 	else:
 		fwcmd = '/sbin/iptables -I ' + chain + ' 1 -s ' + ipaddr + ' -j DROP'
 		print(fwcmd)
+		print(description)
 		#subprocess.call(fwcmd, shell=True)
 		jailed.append(ipaddr)
 		#with open('/var/log/jail/' + now.strftime("%Y%m%d") + ".log",'a+') as flog:
 		#	flog.write(ipaddr + " -> " + chain + "@" + now.strftime("%H:%M:%S"))
 		#	flog.write("\n")
+		#	flog.write(description)
+		#	flog.write("\n")
 
 #define kind of detector or anti detector
-detector = Detector("WebAttack", 3)	
-detector.addPattern(".*UDP flood.*SRC=(.*?) DST=.*",2)
-detector.addPattern(".*Suspected Win exploit.*SRC=(.*?) DST=.*",2)
-detector.addPattern(".*Suspected RST ACK FLAGS.*SRC=(.*?).*",2)
+detector = Detector("WebAttack", 2)	
+detector.addPattern(".*UDP flood.*SRC=(.*?) DST=.*", {"name":"UDP Flood", "score":2})
+detector.addPattern(".*Suspected Win exploit.*SRC=(.*?) DST=.*", {"name":"Win Exploit", "score":2})
+detector.addPattern(".*Suspected RST ACK FLAGS.*SRC=(.*?).*", {"name":"Suspected RST ACK", "score":2})
 
 #define log file to be detect
 logFilePoll = LogFilePoll(detector)
